e382dd6cd33f28572584174fd22641a66ad1241e
Matthew Smith <mattsheridansmith@gmail.com>
2017-10-03 18:28:47 -0400

    seeding file wip

---
+++ server/app.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
+const models = require('../models');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(morgan('dev'));
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: false}));
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.send('Something went wrong' + err.message);
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log('listening on hyunjoo3000');
+  models.db.sync()
+    .then(() => {
+      console.log('synced database');
+    }).catch((err) => console.error('Something is broke.', err, err.stack));
+});
+
